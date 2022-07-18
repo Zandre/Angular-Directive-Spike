4bf146695b17098e118e66f37dbe3e0ba538d9d2
Zandre Botha <botha.zandre@gmail.com>
2022-07-18 10:15:50 +0200

    OG directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'
+import { ButtonTrackDirective } from './shared/directives/og-button-track.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonTrackDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/directives/og-button-track.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostListener} from "@angular/core";
+
+@Directive({
+  selector: 'button'
+})
+
+export class ButtonTrackDirective {
+  constructor(private elementRef: ElementRef) {
+    console.log('[constructor] OG Track Directive');
+    console.info('ElementRef: ', elementRef);
+  }
+
+  @HostListener('click')
+  public onClick(): void {
+    console.log('OG directive clicked');
+  }
+}
